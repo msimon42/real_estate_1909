05ff8383bd7ee16c5c793cd63b6cbbabc783bc3d
msimon42 <msimon@uab.edu>
2019-10-09 09:19:45 -0600

    Add house class

---
+++ lib/house.rb
@@ -0,0 +1,13 @@
+class House
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address, rooms=Array.new)
+    @price = price
+    @address = address
+    @rooms = rooms
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+end

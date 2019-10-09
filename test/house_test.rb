b285a28eb981fde88f92c272ea97e26a6c1f6800
msimon42 <msimon@uab.edu>
2019-10-09 09:30:17 -0600

    Add rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -6,7 +6,10 @@ class HouseTest < MiniTest::Test
 
   def setup
     @house = House.new('400000', '123 Sugar Lane')
-    @room = Room.new(:bedroom, 10, 13)
+    @room1 = Room.new(:bedroom, 10, 13)
+    @room2 = Room.new(:bedroom, 15, 16)
+    @room3 = Room.new(:living_room, 20, 25)
+    @room4 = Room.new(:kitchen, 17, 13)
   end
 
   def test_existence
@@ -14,9 +17,12 @@ class HouseTest < MiniTest::Test
   end
 
   def test_add_room
-    @house.add_room(@room)
+    @house.add_room(@room1)
+    @house.add_room(@room2)
+    @house.add_room(@room3)
+    @house.add_room(@room4)
 
-    assert_equal [@room], @house.rooms
+    assert_equal [@room1, @room2, @room3, @room4], @house.rooms
   end
 end
 

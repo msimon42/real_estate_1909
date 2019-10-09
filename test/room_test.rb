38562c05636c6f5817b103662d60b35bcff27516
msimon42 <msimon@uab.edu>
2019-10-09 09:15:34 -0600

    Add room test

---
+++ test/room_test.rb
@@ -0,0 +1,13 @@
+
+require 'minitest/autorun'
+require_relative '../lib/room'
+
+class RoomTest
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_existence
+    assert_instance_of Room, @room
+  end
+end
